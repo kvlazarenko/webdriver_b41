4d216b688fa2fd3e5f119920e8988e6f67404c86
kvlazarenko <kv.lazarenko@gmail.com>
2023-11-28 20:41:02 +0300

    Задание 7. Сделайте сценарий, проверяющий наличие стикеров у товаров v1

---
+++ java-example/src/test/java/ru/stqa/AvailabilityStickerProduct.java
@@ -0,0 +1,56 @@
+package ru.stqa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class AvailabilityStickerProduct {
+  private WebDriver driver;
+  private WebDriverWait wait;
+  private Properties properties;
+
+  @BeforeEach
+  public void start() throws IOException {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    String target = System.getProperty("target", "local");
+    properties = new Properties();
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+  }
+
+  @Test
+  public void AvailabilityStickerProductTest() {
+
+    driver.get(properties.getProperty("web.baseUrl"));
+    List<WebElement> products = driver.findElements(By.cssSelector("div.image-wrapper"));
+    for (WebElement e : products) {
+      List<WebElement> stickers = e.findElements(By.cssSelector("div.sticker.sale"));
+      boolean a = stickers.size() <= 1;
+      Assertions.assertTrue(a);
+
+    }
+  }
+
+  @AfterEach
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+
+}
+
