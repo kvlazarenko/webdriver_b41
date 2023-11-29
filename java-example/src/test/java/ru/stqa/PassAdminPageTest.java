736e7b760dba3a4d474c791735a7b8347ea846ea
kvlazarenko <kv.lazarenko@gmail.com>
2023-11-29 08:03:23 +0300

    Задание 6. Сделайте сценарий, проходящий по всем разделам админки v3

--- java-example/src/test/java/ru/stqa/PassAdminPageTest.java
+++ java-example/src/test/java/ru/stqa/PassAdminPageTest.java
@@ -4,24 +4,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PassAdminPageTest {
   private WebDriver driver;
@@ -51,20 +45,20 @@ public class PassAdminPageTest {
     driver.findElement(By.name("login")).click();
 
     List<WebElement> listvertical = driver
-            .findElements(By.cssSelector("ul#box-apps-menu a[href^='http://localhost:8080/litecart/admin/?app=']"));
+            .findElements(By.cssSelector("ul#box-apps-menu a[href*='/admin/?app=']"));
 
     for (int i = 0; i <= listvertical.size() - 1; i++) {
       List<WebElement> listverticalrefresh = driver
-              .findElements(By.cssSelector("ul#box-apps-menu a[href^='http://localhost:8080/litecart/admin/?app=']"));
+              .findElements(By.cssSelector("ul#box-apps-menu a[href*='/admin/?app=']"));
       listverticalrefresh.get(i).click();
       driver.findElement(By.cssSelector("td#content h1"));
 
       List<WebElement> listverticalinterior = driver
-              .findElements(By.cssSelector("ul.docs a[href^='http://localhost:8080/litecart/admin/?app=']"));
+              .findElements(By.cssSelector("ul.docs a[href*='/admin/?app=']"));
       if (listverticalinterior.size() > 0) {
         for (int a = 1; a <= listverticalinterior.size() - 1; a++) {
           List<WebElement> listverticalinteriorrefresh = driver
-                  .findElements(By.cssSelector("ul.docs a[href^='http://localhost:8080/litecart/admin/?app=']"));
+                  .findElements(By.cssSelector("ul.docs a[href*='/admin/?app=']"));
           listverticalinteriorrefresh.get(a).click();
           driver.findElement(By.cssSelector("td#content h1"));
           driver.navigate().back();
